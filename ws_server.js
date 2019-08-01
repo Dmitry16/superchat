0bc8dbeb16fdda2edbf2af6fa5e41357ff3bd6d9
Dmitry16 <europartner@inbox.ru>
2019-08-01 10:03:34 +0300

    websocket

---
+++ ws_server.js
@@ -0,0 +1,26 @@
+let WebSocketServer = require('ws').Server;
+let http = require('http');
+let express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+let server = http.createServer(app);
+server.listen('8080');
+
+let WebSocketServer = new WebSocketServer({server: server});
+
+WebSocketServer.once('connection', function(ws) {
+  let timer = setInterval(function() {
+    ws.send(JSON.stringify(process.memoryUsage()), function(err) {
+      if (err) throw err;
+    });
+  }, 100);
+  console.log('client connected!!!');
+
+  ws.on('close', function() {
+    console.log('client disconnected!!!');
+    clearInterval(timer);
+  });
+});
